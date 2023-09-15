22f7b2964880a933605317fbcc61e0cc13058c96
zsh1225 <40027233+zsh1225@users.noreply.github.com>
2023-09-15 18:46:04 +0800

    学习一会儿

--- task1.py
+++ task1.py
@@ -1,4 +1,21 @@
-print("hello")
-a=int(input())
-aaaaaaaaaaaaaaaa
-ok=1
+
+def fibs(num):
+    """
+    This is a Fibnacci function
+    """
+    result = [0, 1]
+    for i in range(num-2):
+        result.append(result[-2] + result[-1])
+    return result  # This is a comment
+
+n=1
+x = y = [1, 2, 3]
+z = [1, 2, 3]
+print(x == y)
+print(x == z)
+print(x is y)
+print(x is z)
+knights = ['we', 'are', 'the', 'knights', 'who', 'we','say', 'ni']
+knights.index('we')
+knights.index('we', 5)
+knights[4] # see the difference
