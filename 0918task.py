55ad8568ce964f13a6c761b5c5973208592594af
zsh1225 <18994233559@189.cn>
2023-09-21 20:51:24 +0800

    a

--- 0918task.py
+++ 0918task.py
@@ -1,5 +1,14 @@
-中文 = f'字符串类型'
-print('{}'.format(中文))
-def fun(a=1,b=1,*c):
-    return c
-print(fun(2,3,4))
+# 中文 = f'字符串类型'
+# print('{}'.format(中文))
+# def fun(a=1,b=1,*c):
+#     return c
+# print(fun(2,3,4))
+
+class class_demo():
+    def __init__(self):
+        self.a=10
+ss=class_demo()
+print(ss.a)
+
+
+# print()
