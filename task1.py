303ffaadf19d1f38b4c28239346f695f65a6137e
zsh1225 <40027233+zsh1225@users.noreply.github.com>
2023-10-25 17:59:32 +0800

    a

--- task1.py
+++ task1.py
@@ -1,20 +1,22 @@
-def fibs(num):
-    """
-    This is a Fibnacci function
-    """
-    result = [0, 1]
-    for i in range(num-2):
-        result.append(result[-2] + result[-1])
-    return result  # This is a comment
+# def fibs(num):
+#     """
+#     This is a Fibnacci function
+#     """
+#     result = [0, 1]
+#     for i in range(num-2):
+#         result.append(result[-2] + result[-1])
+#     return result  # This is a comment
 
-n=1
-x = y = [1, 2, 3]
-z = [1, 2, 3]
-print(x == y)
-print(x == z)
-print(x is y)
-print(x is z)
-knights = ['we', 'are', 'the', 'knights', 'who', 'we','say', 'ni']
-knights.index('we')
-knights.index('we', 5)
-knights[4] # see the difference
+# n=1
+# x = y = [1, 2, 3]
+# z = [1, 2, 3]
+# print(x == y)
+# print(x == z)
+# print(x is y)
+# print(x is z)
+# knights = ['we', 'are', 'the', 'knights', 'who', 'we','say', 'ni']
+# knights.index('we')
+# knights.index('we', 5)
+# knights[4] # see the difference
+f=open(r"E:\\github\\learnCPP\shujujiegou\\Hotel2.txt")
+print(f.read())
